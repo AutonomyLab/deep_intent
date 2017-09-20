3981ef05483c113fe9301e18acdd37878c41de70
pratikgujjar <pratikgujjar007@gmail.com>
2017-09-19 20:13:34 -0700

    Adding a template webpage

--- code/jaad_config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Where JAAD data will be saved if you run process_jaad.py
-DATA_DIR = '/local_home/JAAD_Dataset/'
-
-RESIZED_IMGS_DIR = '/local_home/JAAD_Dataset/resized_imgs_128'
-if not os.path.exists(RESIZED_IMGS_DIR):
-    os.mkdir(RESIZED_IMGS_DIR)
