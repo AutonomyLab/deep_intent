c879f64914c9827f7ab9c9ec7bcad5e35b423444
pratikgujjar <pratikgujjar007@gmail.com>
2018-07-18 19:42:42 -0700

    Graph code added

---
+++ code/autoencoder_model/scripts/thesis_scripts/config_3d.py
@@ -0,0 +1,101 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from keras.optimizers import SGD
+from keras.optimizers import Adam
+from keras.optimizers import adadelta
+from keras.optimizers import rmsprop
+from keras.layers import Layer
+from keras import backend as K
+K.set_image_dim_ordering('tf')
+import socket
+import os
+
+# -------------------------------------------------
+# Background config:
+hostname = socket.gethostname()
+if hostname == 'baymax':
+    path_var = 'baymax/'
+elif hostname == 'walle':
+    path_var = 'walle/'
+elif hostname == 'bender':
+    path_var = 'bender/'
+else:
+    path_var = 'zhora/'
+
+DATA_DIR= '/local_home/JAAD_Dataset/iros/resized_imgs_208_thesis/train/'
+
+VAL_DATA_DIR= '/local_home/JAAD_Dataset/iros/resized_imgs_208_thesis/val/'
+
+TEST_DATA_DIR= '/local_home/JAAD_Dataset/iros/resized_imgs_208_thesis/test/'
+# TEST_DATA_DIR= '/local_home/JAAD_Dataset/fun_experiments/resized/'
+
+RESULTS_DIR = '/local_home/JAAD_Dataset/thesis/results/conv/'
+
+# MODEL_DIR = './../' + path_var + 'models'
+MODEL_DIR = RESULTS_DIR + 'models/'
+if not os.path.exists(MODEL_DIR):
+    os.mkdir(MODEL_DIR)
+
+# CHECKPOINT_DIR = './../' + path_var + 'checkpoints'
+CHECKPOINT_DIR = RESULTS_DIR + 'checkpoints/'
+if not os.path.exists(CHECKPOINT_DIR):
+    os.mkdir(CHECKPOINT_DIR)D49A6A
+
+GEN_IMAGES_DIR = RESULTS_DIR + 'generated_images/'
+if not os.path.exists(GEN_IMAGES_DIR):
+    os.mkdir(GEN_IMAGES_DIR)
+
+LOG_DIR = RESULTS_DIR + 'logs/'
+if not os.path.exists(LOG_DIR):
+    os.mkdir(LOG_DIR)
+
+TF_LOG_DIR = RESULTS_DIR + 'tf_logs/'
+if not os.path.exists(TF_LOG_DIR):
+    os.mkdir(TF_LOG_DIR)
+
+TEST_RESULTS_DIR = RESULTS_DIR + 'test_results/'
+if not os.path.exists(TEST_RESULTS_DIR):
+    os.mkdir(TEST_RESULTS_DIR)
+
+PRINT_MODEL_SUMMARY = True
+SAVE_MODEL = True
+PLOT_MODEL = True
+SAVE_GENERATED_IMAGES = True
+SHUFFLE = True
+VIDEO_LENGTH = 32
+IMG_SIZE = (128, 208, 3)
+RAM_DECIMATE = False
+REVERSE = True
+
+
+# -------------------------------------------------
+# Network configuration:
+print ("Loading network/training configuration.")
+print ("Config file: " + str(__name__))
+
+BATCH_SIZE = 9
+TEST_BATCH_SIZE = 1
+NB_EPOCHS_AUTOENCODER = 30
+
+# OPTIM_A = Adam(lr=0.0001, beta_1=0.5)
+OPTIM_A = rmsprop(lr=0.0001, rho=0.9)
+OPTIM_B = rmsprop(lr=0.00001, rho=0.9)
+# OPTIM_A = SGD(lr=0.000001, momentum=0.5, nesterov=True)
+
+lr_schedule = [7, 14, 20, 30]  # epoch_step
+
+def schedule(epoch_idx):
+    if (epoch_idx) <= lr_schedule[0]:
+        return 0.001
+    elif (epoch_idx) <= lr_schedule[1]:
+        return 0.0001  # lr_decay_ratio = 10
+    elif (epoch_idx) <= lr_schedule[2]:
+        return 0.00001  # lr_decay_ratio = 10
+    elif (epoch_idx) <= lr_schedule[3]:
+        return 0.00001
+    return 0.00001
+
+
+
--- code/autoencoder_model/scripts/thesis_scripts/plot_results.py
+++ code/autoencoder_model/scripts/thesis_scripts/plot_results.py
@@ -4,15 +4,19 @@ from __future__ import print_function
 
 import numpy as np
 import numpy.random
+import math
 import argparse
 import matplotlib
+from matplotlib import rc
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+import matplotlib.patches as mpatches
 import cv2
 import ast
+import json
 import os
-from config_r16 import TEST_RESULTS_DIR
+# from config_3d import TEST_RESULTS_DIR
 
 def plot_heatmap(attn_layer, epoch, vid_num, file):
     print (attn_layer)
@@ -50,12 +54,183 @@ def plot_err_variation(values, index, dc_value, filename):
                 transparent=False)
 
 
-def errorbars(data):
-    means = np.mean(data, axis=0)
-    l_error = abs(means-np.min(data, axis=0))
-    h_error = abs(means-np.max(data, axis=0))
+def errorbars(data, model, colour='#3E6386'):
+
     plt.clf()
-    plt.errorbar()
+
+    # Setup plot appearances
+    rc('text', usetex=True)
+    rc('font', **{'family': 'serif', 'serif': ['Computer Modern'],
+                 'monospace':['Computer Modern']})
+
+    plt.rcParams['font.size'] = 14
+    plt.rcParams['axes.labelsize'] = 10
+    plt.rcParams['axes.labelweight'] = 'bold'
+    plt.rcParams['axes.linewidth'] = 0.5
+    plt.rcParams['xtick.labelsize'] = 12
+    plt.rcParams['ytick.labelsize'] = 12
+    plt.rcParams['legend.fontsize'] = 12
+    plt.rcParams['figure.titlesize'] = 14
+    plt.rcParams['grid.linestyle'] = '--'
+    plt.rcParams['grid.linewidth'] = 0.6
+    plt.rcParams['grid.color'] = [0.8, 0.8, 0.8]
+    # plt.style.use('seaborn-white')
+
+    meanErr = np.mean(data, axis=0)
+    frames = np.linspace(1, 16, 16)
+    stdDevs = np.std(data, axis=0)
+    dcVal = [meanErr[-1]] * len(meanErr[0:16])
+    dcDev = [stdDevs[-1]] * len(stdDevs[0:16])
+
+    dcLine = plt.plot(frames, dcVal)
+    plt.setp(dcLine, color='#D49A6A', linewidth=1.5, linestyle='-.')
+    # plt.fill_between(frames, [a - b for a, b in zip(dcVal, dcDev)],
+    #                  [a + b for a, b in zip(dcVal, dcDev)], facecolor="#C986AF", alpha=0.7)
+
+
+    lines = plt.plot(frames, meanErr[0:16], 'o')
+    plt.setp(lines, color=colour, linewidth=1, linestyle='-')
+    plt.fill_between(frames, [a - b for a, b in zip(meanErr[0:16], stdDevs[0:16])],
+                     [a + b for a, b in zip(meanErr[0:16], stdDevs[0:16])], facecolor="#DBF4FF", alpha=0.7)
+
+    ax = plt.gca()
+    ax.spines['bottom'].set_color('white')
+    ax.spines['top'].set_color('white')
+    ax.spines['right'].set_color('white')
+    ax.spines['left'].set_color('white')
+    # ax.set_yticks(np.arange(0, math.ceil(max(meanErr[0:16])), 0.08))
+    # plt.grid()
+    ax.yaxis.grid()
+    # ax.xaxis.grid()
+
+    plt.xlabel('Predicted frame numbers', fontsize=14)
+    plt.ylabel('$l_{1}$ loss', fontsize=14)
+    plt.title("Temporal $l_{1}$ loss Variation")
+
+    ax.set_facecolor([1, 1, 1])
+    plt.savefig('/local_home/JAAD_Dataset/thesis/plots/' + model + '-tem.pdf', bbox_inches='tight')
+
+
+def errorbars_all(models, y, colours):
+
+    plt.clf()
+    # Setup plot appearances
+    rc('text', usetex=True)
+    rc('font', **{'family': 'serif', 'serif': ['Computer Modern'],
+                 'monospace':['Computer Modern']})
+
+    plt.rcParams['font.size'] = 14
+    plt.rcParams['axes.labelsize'] = 10
+    plt.rcParams['axes.labelweight'] = 'bold'
+    plt.rcParams['axes.linewidth'] = 0.5
+    # plt.rcParams['axes.color'] = 'white'
+    plt.rcParams['xtick.labelsize'] = 12
+    plt.rcParams['ytick.labelsize'] = 12
+    plt.rcParams['legend.fontsize'] = 12
+    plt.rcParams['figure.titlesize'] = 14
+    plt.rcParams['grid.linestyle'] = '--'
+    plt.rcParams['grid.linewidth'] = 0.6
+    plt.rcParams['grid.color'] = [0.8, 0.8, 0.8]
+    # plt.style.use('seaborn-white')
+
+    for i in range(len(models)):
+        data = np.load('/local_home/JAAD_Dataset/thesis/plots/data/mae/1256_mae_' + models[i].lower() + '.npy')
+
+        meanErr = np.mean(data, axis=0)
+        frames = np.linspace(1, 16, 16)
+        stdDevs = np.std(data, axis=0)
+
+        lines = plt.plot(frames, meanErr[0:16], 'o')
+        plt.setp(lines, color=colours[i], linewidth=1, linestyle='-')
+        plt.text(16.5, y[i], models[i].lower(), fontsize=14, color=colours[i])
+
+        # plt.fill_between(frames, [a - b for a, b in zip(meanErr[0:16], stdDevs[0:16])],
+        #                  [a + b for a, b in zip(meanErr[0:16], stdDevs[0:16])], facecolor="#DBF4FF", alpha=0.7)
+
+    ax = plt.gca()
+    ax.spines['bottom'].set_color('white')
+    ax.spines['top'].set_color('white')
+    ax.spines['right'].set_color('white')
+    ax.spines['left'].set_color('white')
+    # ax.set_yticks(np.arange(0, math.ceil(max(meanErr[0:16])), 0.08))
+    # plt.grid()
+    ax.yaxis.grid()
+    # ax.xaxis.grid()
+
+    plt.xlabel('Predicted frame numbers', fontsize=14)
+    plt.ylabel('$l_{1}$ loss', fontsize=14)
+    plt.title("Temporal $l_{1}$ loss Variation")
+
+    # red_patch = mpatches.Patch(color='#3E6386', label='With Switching')
+    # blue_patch = mpatches.Patch(color='#430029', label='Without Switching')
+    # plt.legend(handles=[red_patch])
+    # ax.set_facecolor([0.975, 0.975, 0.975])
+    # ax.set_facecolor([0.99, 0.99, 0.99])
+    ax.set_facecolor([1, 1, 1])
+    plt.savefig('/local_home/JAAD_Dataset/thesis/plots/all-tem.pdf', bbox_inches='tight')
+
+
+def trainval_plot(data, model, colour='#3E6386'):
+
+    plt.clf()
+
+    # Setup plot appearances
+    rc('text', usetex=True)
+    rc('font', **{'family': 'serif', 'serif': ['Computer Modern'],
+                 'monospace':['Computer Modern']})
+
+    plt.rcParams['font.size'] = 14
+    plt.rcParams['axes.labelsize'] = 10
+    plt.rcParams['axes.labelweight'] = 'bold'
+    plt.rcParams['axes.linewidth'] = 0.5
+    plt.rcParams['xtick.labelsize'] = 12
+    plt.rcParams['ytick.labelsize'] = 12
+    plt.rcParams['legend.fontsize'] = 12
+    plt.rcParams['figure.titlesize'] = 14
+    plt.rcParams['grid.linestyle'] = '--'
+    plt.rcParams['grid.linewidth'] = 0.6
+    plt.rcParams['grid.color'] = [0.8, 0.8, 0.8]
+    # plt.style.use('seaborn-white')
+
+    trainLoss = []
+    valLoss = []
+    for i in range (len(data)):
+        line = json.loads(data[i])
+        trainLoss.append(line['train_loss'])
+        valLoss.append(line['val_loss'])
+
+    epochs = np.linspace(21, 30, 10)
+
+    valLine = plt.plot(epochs, valLoss[20:], 'o')
+    plt.setp(valLine, color='#D49A6A', linewidth=1.5, linestyle='-')
+    # plt.text(20.5, valLoss[19], 'Validation Loss', fontsize=14, color=colour)
+
+
+    trainLine = plt.plot(epochs, trainLoss[20:], 'o')
+    plt.setp(trainLine, color=colour, linewidth=1, linestyle='-')
+    # plt.text(20.5, trainLoss[19], 'Training Loss', fontsize=14, color='#D49A6A')
+
+    train_patch = mpatches.Patch(color=colour, label='Training Loss')
+    val_patch = mpatches.Patch(color='#D49A6A', label='Validation Loss')
+    plt.legend(handles=[train_patch, val_patch])
+
+    ax = plt.gca()
+    ax.spines['bottom'].set_color('white')
+    ax.spines['top'].set_color('white')
+    ax.spines['right'].set_color('white')
+    ax.spines['left'].set_color('white')
+    # ax.set_xticks(epochs+1)
+    # plt.grid()
+    ax.yaxis.grid()
+    # ax.xaxis.grid()
+
+    plt.xlabel('Epochs', fontsize=14)
+    plt.ylabel('$l_{1}$ loss', fontsize=14)
+    # plt.title("Training and Validation Losses")
+
+    ax.set_facecolor([1, 1, 1])
+    plt.savefig('/local_home/JAAD_Dataset/thesis/plots/' + model.lower() + '-train-val-mse.pdf', bbox_inches='tight')
+
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -72,68 +247,46 @@ if __name__ == "__main__":
     # print (type(args.attn_layers))
     # print (args.epoch)
     # print (args.vid_num)
-    plot_heatmap(attn_layer=args.attn_layers, epoch=args.epoch, vid_num=args.vid_num, file=args.file)
+    # plot_heatmap(attn_layer=args.attn_layers, epoch=args.epoch, vid_num=args.vid_num, file=args.file)
 
-# res = np.load('../DRRK16/test_results/graphs/values/1256_mae.npy')
-# rescheck = np.load('../NRNK16/test_results/graphs/values/1256_mae.npy')
-# dilation = np.load('../NRRK16/test_results/graphs/values/1256_mae.npy')
-# kernel = np.load('../NRNN16/test_results/graphs/values/1256_mae.npy')
-# rev = np.load('../DRNK16/test_results/graphs/values/1256_mae.npy')
-# res_mean = np.mean(res, axis=0)
-# rescheck_mean = np.mean(rescheck, axis=0)
-# dilation_mean = np.mean(dilation, axis=0)
-# kernel_mean = np.mean(dilation, axis=0)
-# rev_mean = np.mean(rev, axis=0)
+    models = ['Res', 'Rescheck', 'Rendec', 'Kernel', 'Dilation', 'Rev']
+    colours = ['#58B33D', '#AB78CC', '#5571B9', '#C43731', '#EC8F1B', '#E394D4']
 
-# import math
-# import numpy as np
-# import matplotlib.pyplot as plt
-# import matplotlib.patches as mpatches
-#
-# # def myreadlines(f, newline):
-# #   buf = ""
-# #   while True:
-# #     while newline in buf:
-# #       pos = buf.index(newline)
-# #       yield buf[:pos]
-# #       buf = buf[pos + len(newline):]
-# #     chunk = f.read(4096)
-# #     if not chunk:
-# #       yield buf
-# #       break
-# #     buf += chunk
-#
-# # plt.rcParams['font.family'] = 'serif'
-# plt.rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
-# plt.rc('text', usetex=True)
-#
-# # plt.rcParams['font.serif'] = 'Ubuntu'
-# # plt.rcParams['font.monospace'] = 'Ubuntu Mono'
-# plt.rcParams['font.size'] = 10
-# plt.rcParams['axes.labelsize'] = 10
-# plt.rcParams['axes.labelweight'] = 'bold'
-# plt.rcParams['xtick.labelsize'] = 8
-# plt.rcParams['ytick.labelsize'] = 8
-# plt.rcParams['legend.fontsize'] = 10
-# plt.rcParams['figure.titlesize'] = 12
-# plt.style.use('seaborn-white')
-#
-# #   if (prefixStr == "Results/200Robots500Boxes/star_switch/star_switch"):
-# #     lines = plt.plot(stepNumbers, avgAvgDists)
-# #     plt.setp(lines, color='#3E6386', linewidth=1)
-# #     plt.fill_between(stepNumbers, [a-b for a,b in zip(avgAvgDists, stdDevs)], [a+b for a,b in zip(avgAvgDists, stdDevs)], facecolor="#9DB3C8", alpha=0.7)
-# #   else:
-# #     lines = plt.plot(stepNumbers, avgAvgDists)
-# #     plt.setp(lines, color='#430029', linewidth=1)
-# #     plt.fill_between(stepNumbers, [a-b for a,b in zip(avgAvgDists, stdDevs)], [a+b for a,b in zip(avgAvgDists, stdDevs)], facecolor="#C986AF", alpha=0.7)
-# #   ax = plt.gca()
-# #   ax.set_yticks(np.arange(0, math.ceil(max(avgAvgDists)), 0.50))
-# # plt.xlabel('World Steps')
-# # plt.ylabel('Mean Box Distance from Goal Shape (m)')
-# # plt.title("Avg Distance from Boxes to \nStar Shaped Goal with and without Switching\n200 Robots 500 Boxes")
-# # #plt.fill_between(stepNumbers, [a-b for a,b in zip(avgAvgDistsOut, stdDevsOut)], [a+b for a,b in zip(avgAvgDistsOut, stdDevsOut)], facecolor="#9DB3C8")
-# # red_patch = mpatches.Patch(color='#3E6386', label='With Switching')
-# # blue_patch = mpatches.Patch(color='#430029', label='Without Switching')
-# # plt.legend(handles=[blue_patch, red_patch])
-# # ax.set_facecolor([0.975, 0.975, 0.975])
-# # plt.savefig('/home/abignell/Pictures/push_pics/starstyle.pdf', bbox_inches='tight')
+
+    # 98766B
+    # D5D5D5
+    # B2C900
+
+    # for i in range (len(models)):
+    #     data = np.load('/local_home/JAAD_Dataset/thesis/plots/data/mae/1256_mae_' + models[i] + '.npy')
+    #     errorbars(data, models[i])
+
+
+    # y = []
+    # for model in models:
+    #     data = np.load('/local_home/JAAD_Dataset/thesis/plots/data/mae/1256_mae_' + model.lower() + '.npy')
+    #     meanErr = np.mean(data, axis=0)
+    #     y_val = meanErr[15]
+    #     if model == 'Rescheck':
+    #         y_val = y_val + 0.0015
+    #     if model == 'Rev':
+    #         y_val = y_val + 0.001
+    #     if model == 'Kernel':
+    #         y_val = y_val - 0.001
+    #     if model == 'Dilation':
+    #         y_val = y_val - 0.0035
+    #     if model == 'Res':
+    #         y_val = y_val - 0.006
+    #     if model == 'Rendec':
+    #         y_val = y_val - 0.0055
+    #
+    #     y.append(y_val)
+    #
+    # errorbars_all(models, y, colours=colours)
+
+    for i in range (len(models)):
+        with open('/local_home/JAAD_Dataset/thesis/plots/data/train-val_loss/losses_gen_' + models[i].lower() + '.json', 'r') as f:
+            data = f.readlines()
+        trainval_plot(data, models[i])
+
+
